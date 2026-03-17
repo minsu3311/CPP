1475a2a786ce2e365a93dc1e69f89b246d0c7f9b
minsu3311 <chssal1792@gmail.com>
2026-03-17 18:16:03 +0900

    Refactor output formatting and add additional problems

--- Weak3/97_1.cpp
+++ Weak3/97_1.cpp
@@ -1,7 +1,8 @@
 #include<iostream>
 
 int main()
-{
+{   
+    printf("1번 문제\n");
     for(int i = 1; i <= 5; i++)
     {
       for(int j = 1; j <= i; j++)
@@ -11,6 +12,8 @@ int main()
 
         std:: cout << "\n";
     }
+        std:: cout << "\n";
+        printf("2번 문제\n");
     for(int j = 1; j <= 5; j++)
     {
     for(int i = 5; i >= j; --i)
@@ -19,4 +22,40 @@ int main()
     }
     std::cout << "\n";
     }
+        printf("\n3번 문제\n");
+
+    for(int i = 1; i <= 5; i++) 
+    {
+        for(int k = 1; k <= 5 - i; k++)
+        {
+            std::cout << "  "; 
+        }
+        for(int j = i; j >= 1; j--)
+        {
+            std::cout << j << " ";
+        }
+        std::cout << "\n";
+    }
+
+
+            printf("\n4번 문제\n");
+    for(int i = 1; i <= 5; i++)
+    {
+        for(int k = 1; k <= 5 - i; k++)
+        {
+            std::cout << "  ";
+        }
+
+        for(int j = i; j >= 1; j--)
+        {
+            std::cout << j << " ";
+        }
+
+        for(int m = 2; m <= i; m++)
+        {
+            std::cout << m << " ";
+        }
+        std::cout << "\n";
+    }
 }
+
